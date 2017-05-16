c478801fcc7cb67fde978a4651aef5513198f858
Jonathan Prairie <augustus.seizure.1@gmail.com>
2017-05-15 23:02:12 -0400

    removing white lines between squares, snapping squares to edge of screen

--- hex.js
+++ hex.js
@@ -1,88 +1,270 @@
-var c = document.getElementById('c');
-var c2 = c.getContext('2d');
+var c = document.getElementById('bg');
+var ctx = c.getContext('2d');
 
-c.height = window.innerHeight-15;
-c.width  = window.innerWidth-15;
+c.height = window.innerHeight;
+c.width  = window.innerWidth;
 
-c2.shadowBlur = 5;
-c2.globalAlpha = .6;
-//c2.shadowColor = 'rgb(0, 0, 0)';
+var hexAngle = Math.PI / 3,
+    hexCos = Math.cos(hexAngle),
+    hexSin = Math.sin(hexAngle);
 
 function randInt(start=1, end=100) {
-	return Math.floor((end - start) * Math.random()) + start;
+    return Math.floor((end - start) * Math.random()) + start;
 }
 
-function drawHex(x, y, size = 100, color = 0) {
-	if (color == 0) {
-  	var colorR = (randInt(256)).toString(16);
-    var colorG = (randInt(256)).toString(16);
-    var colorB = (randInt(256)).toString(16);
-    color = '#' + colorR + colorG + colorB; 
-    c2.shadowColor = color;
-  }
-  c2.fillStyle = color;
-  c2.moveTo(x, y);
-  c2.beginPath();
-  c2.lineTo(x+size, y);
-  c2.lineTo(x+size+size*Math.cos(Math.PI/3), y+size*Math.sin(Math.PI/3));
-  c2.lineTo(x+size, y+2*size*Math.sin(Math.PI/3));
-  c2.lineTo(x, y+2*size*Math.sin(Math.PI/3));
-  c2.lineTo(x-size*Math.cos(Math.PI/3), y+size*Math.sin(Math.PI/3));
-  c2.lineTo(x, y);
-  c2.closePath();
-  c2.fill();
-}
+/** 
+ * generates and returns an array of colors that (hopefully) look good together
+ */
+function randColorSeq(numColors, colorSeed=2*Math.PI*Math.random()) {
+    let colors = [];
 
-function stringHex(x=0, y=0, size = 100) {
-	var c = document.getElementById('c'),
-  		hexAngle = Math.PI/3,
-      hexCos = Math.cos(hexAngle),
-      hexSin = Math.cos(hexAngle),
-      scaledHexCos = size * hexCos,
-      scaledHexSin = size * hexSin,
-      bufferX = size / 5,
-      bufferY = size / 2.5,
-      nextHexUpProb = .5,
-      nextHexUpLimit = .2,
-			nextHexProbStack = .1,
-      nextHex;
-  
-  for(i=0; x < c.width; ++i) {
-  
-    setTimeout(function (a, b, c) {
-    	return function() {drawHex(a, b, c);};
-		}(x, y, size), i*500);
-    
-    if (y < 0) {
-      nextHex = -1;
+    for(var i=0; i<numColors; ++i) {
+	colorSeed += 2 * Math.PI / 3;
+	colors.push('#' + (Math.cos(colorSeed) * 127 + 128 << 16
+			   | Math.cos(colorSeed + 2 * Math.PI / 3) * 127 + 128 << 8
+			   | Math.cos(colorSeed + Math.PI / 3) * 127 + 128).toString(16));
     }
-    else if(y > c.height) {
-      nextHex = 1;
+
+    return colors;
+}
+
+function genHuesFromAngleSpread(numColors, spread, angle) {
+    let deltaAngle = spread / numColors,
+	hues = [];
+
+    for(var i=0; i<numColors; ++i) {
+	hues.push((angle + i * deltaAngle) % 1);
+    }
+
+    return hues;
+}
+
+/** 
+  * paramaters:
+  *   h = [0, 1] hue 
+  *   s = [0, 1] saturation
+  *   l = [0, 1] lightness 
+  *
+  * returns:
+  *   r = [0, 255] red
+  *   g = [0, 255] green
+  *   b = [0, 255] blue
+  */
+function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        };
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+/**
+  * paramaters:
+  *   rgbArray = [r, g, b], r/g/b belonging to [0, 255]
+  *
+  * returns:
+  *   rgb hex value in the form '#rrggbb'
+  */
+function rgbArrayToHex(rgbArray) {
+    return '#' + (rgbArray[0] << 16 | rgbArray[1] << 8 | rgbArray[2]).toString(16);
+}
+
+/**
+  * paramaters:
+  *   colors = array of rgb hex values in the form '#rrggbb'
+  *
+  * side effects:
+  *   draw color swatches across window
+  */
+function drawColorSwatches(colors, sqrsPerRow, sqrWidth, sqrHeight) {
+    let x = 0,
+	y = 0,
+	tempAlpha = ctx.globalAlpha;
+    ctx.globalAlpha = 1;
+    for(var i=0; i<colors.length; ++i) {
+	ctx.fillStyle = colors[i];
+	ctx.fillRect(x,y,sqrWidth,sqrHeight);
+	if (i != 0 && i % sqrsPerRow == 0) {
+	    y += sqrHeight - 1;
+	    x = 0;
+	}
+	else {
+	    x += sqrWidth - 1;
+	}
+    }
+    ctx.globalAlpha = tempAlpha;
+}
+
+function hex(x, y, size, color) {
+    return {
+	"x": x,
+	"y": y,
+	"size": size,
+	"color": color
+    };
+}
+
+function hexSnake(len, size, color, x, y) {
+    return {
+	"hexes": buildHexSnake(len, size, color, x, y),
+	"len": len,
+	"size": size,
+	"color": color,
+	"x": x,
+	"y": y
+    };
+}
+
+function buildHexSnake(len, size, color, x, y) {
+    hexes = [];
+    hexes.push(hex(x, y, size, color));
+    for(let i=1; i<len; ++i) {
+	hexes.push(getNextHex(hexes[i-1]));
+    }
+    return hexes;
+}
+
+function getNextHex(h, newColor=undefined) {
+    var scaledHexCos = h.size * hexCos,
+	scaledHexSin = h.size * hexSin,
+	bufferX = h.size / 5,
+	bufferY = h.size / 2.5,
+	nextHexUpProb = .5,
+	nextHexUpLimit = .2,
+	nextHexProbStack = .1,
+	nextHex;
+
+    if (h.y < 0) {
+	nextHex = -1;
+    }
+    else if(h.y > c.height) {
+	nextHex = 1;
     }
     else if(Math.random() < nextHexUpProb) {
-      nextHex = 1;
+	nextHex = 1;
     }
     else {
-      nextHex = -1;
+	nextHex = -1;
     }
     
-		if (nextHex == 1 && nextHexUpProb < (1-nextHexUpLimit)) {
-    	nextHexUpProb += ( (1 - nextHexUpProb) * nextHexProbStack);
+    if (nextHex == 1 && nextHexUpProb < (1-nextHexUpLimit)) {
+	nextHexUpProb += ( (1 - nextHexUpProb) * nextHexProbStack);
     }
     else if (nextHexUpProb < nextHexUpLimit) {
-    	nextHexUpProb -= (nextHexUpProb * nextHexProbStack);
+	nextHexUpProb -= (nextHexUpProb * nextHexProbStack);
     }
-  
-	  x = x + size + scaledHexCos + bufferX;
-  	y = y + nextHex * scaledHexSin + nextHex * bufferY;
-  }
-  
-  //drawHex(x, y, size);
+    
+    nextX = h.x + h.size + scaledHexCos + bufferX;
+    nextY = h.y + nextHex * scaledHexSin + nextHex * bufferY;
+
+    return hex(nextX, nextY, h.size, newColor ? newColor : h.color);
 }
 
-s = 15
-h = randInt(c.height)
+function drawHex(h) {
+    ctx.fillStyle = h.color;
+    ctx.moveTo( h.x, h.y );
+    ctx.beginPath();
+    ctx.lineTo( h.x + h.size, h.y );
+    ctx.lineTo(
+	h.x + h.size + h.size * hexCos,
+	h.y + h.size * hexSin
+    );
+    ctx.lineTo(
+	h.x + h.size,
+	h.y + 2 * h.size * hexSin
+    );
+    ctx.lineTo(
+	h.x,
+	h.y + 2 * h.size * hexSin
+    );
+    ctx.lineTo(
+	h.x - h.size * hexCos,
+	h.y + h.size * hexSin
+    );
+    ctx.lineTo(h.x, h.y);
+    ctx.closePath();
+    ctx.fill();
+}
 
-stringHex(0, h, s);
-stringHex(0, h, s);
-stringHex(0, h, s);
+function clearCanvas() {
+    ctx.clearRect(0, 0, c.width, c.height);
+}
+
+function randHexSnakes(numSnakes) {
+    let snakes = [],
+	colors = randColorSeq(numSnakes);
+
+    for(let i=0; i<numSnakes; ++i) {
+	snakes.push(
+	    hexSnake(
+		randInt(3, 5),
+		randInt(15, 250),
+		colors[i],
+		randInt(0, c.width),
+		randInt(0, c.height)
+	    )
+	);
+    }
+    return snakes;
+}
+
+function setAlphaFromSize(size) {
+    // ctx.globalAlpha = .3 + (1 / size);
+    ctx.globalAlpha = 1 - (.7 / (1 + (50/size)));
+}
+
+function drawHexSnake() {
+    hexSnakes = randHexSnakes(3);
+    hexSnakes.forEach(
+	function(snake) {
+	    setAlphaFromSize(snake.size);
+	    ctx.shadowBlur = 20;
+	    ctx.shadowColor = "black"; 
+	    snake.hexes.forEach(drawHex);
+	}
+    );
+    return hexSnakes;
+}
+
+function hexSnakesBG() {
+    for(let i=0; i<2; ++i) {
+	hexSnakesBG();
+    }
+}
+
+function flatSquaresBG() {
+    let numSquaresWide = 16,
+	totalNumSquares = (Math.floor(c.height * numSquaresWide / c.width) + 1) * numSquaresWide + 1,
+	colorWheelSpread = .4,
+	sqrWidth = c.width / numSquaresWide + 1,
+	sqrHeight = sqrWidth + (c.height % sqrWidth) / Math.floor(c.height / sqrWidth) + 1,
+	startColorAngle = Math.random(),
+	saturation = () => randInt(3000, 5000) / 10000,
+	lightness = () => randInt(3000, 7000) / 10000,
+	colors = ((seq) => randInt(0, 2) ? seq : seq.reverse()) (
+		    genHuesFromAngleSpread(totalNumSquares, colorWheelSpread, startColorAngle)
+			.map(x => hslToRgb(x, saturation(), lightness()))
+			.map(rgbArrayToHex)
+		 );
+	
+    drawColorSwatches(colors, numSquaresWide, sqrWidth, sqrHeight);
+}
+		    
+flatSquaresBG();
